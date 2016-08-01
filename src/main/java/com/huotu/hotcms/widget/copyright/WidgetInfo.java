a1fb1c38819cb12d4088b9d0c1aaf0c788f01941
lihuaixin <1072931117@qq.com>
2016-08-01 20:38:24 +0800

    name description 修改

--- src/main/java/com/huotu/hotcms/widget/copyright/WidgetInfo.java
+++ src/main/java/com/huotu/hotcms/widget/copyright/WidgetInfo.java
@@ -54,20 +54,16 @@ public class WidgetInfo implements Widget{
 
     @Override
     public String name(Locale locale) {
-        if (locale.equals(Locale.CHINESE)) {
+        if (locale.equals(Locale.CHINA)) {
             return "版权信息";
         }
         return "copyright";
     }
 
-    @Override
-    public String description() {
-        return "这是一个版权信息，你可以对组件进行自定义修改。";
-    }
 
     @Override
     public String description(Locale locale) {
-        if (locale.equals(Locale.CHINESE)) {
+        if (locale.equals(Locale.CHINA)) {
             return "这是一个版权信息，你可以对组件进行自定义修改。";
         }
         return "This is a copyright,  you can make custom change the component.";
